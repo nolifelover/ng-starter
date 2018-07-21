fd25f1084c8054a43a7497c57641647a3a2e6da6
todsaporn sangboon <nolifelover@gmail.com>
2018-07-21 14:45:51 +0700

    ng template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotonComponent } from './jumboton/jumboton.component';
+import { FeatureComponent } from './feature/feature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    NavbarComponent,
+    JumbotonComponent,
+    FeatureComponent
   ],
   imports: [
     BrowserModule,
