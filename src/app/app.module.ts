5e0e8fb5327900c56a5dcbf5035fd88ace959009
todsaporn sangboon <nolifelover@gmail.com>
2018-07-21 16:16:03 +0700

    using http client with mocky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { JumbotonComponent } from './jumboton/jumboton.component';
 import { FeatureComponent } from './feature/feature.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +18,15 @@ import { FeatureComponent } from './feature/feature.component';
     ContactComponent,
     NavbarComponent,
     JumbotonComponent,
-    FeatureComponent
+    FeatureComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
 
 @Component({
   selector: 'app-feature',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feature.component.css']
 })
 export class FeatureComponent implements OnInit {
-
-  constructor() { }
+  datas = [];
+  show = false;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get("http://www.mocky.io/v2/5b52f5322f0000fb0a3bb672?mocky-delay=2000ms").subscribe((data:any)=>{
+      this.datas = data.datas;
+      this.show = true;
+      console.log(data);
+    })
   }
 
 }
